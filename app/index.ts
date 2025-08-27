d3953661b0489090953b19315dca07c90c022463
Mathasit Jaihow <mathasit-j@rmutp.ac.th>
2025-08-27 14:56:25 +0700

    add script

--- app/index.ts
+++ app/index.ts
@@ -4,6 +4,6 @@ const app = new Hono();
 
 app.get('/', (c) => c.text('Hono!'));
 
-app.get('/about', (c) => {return c.json({message: "Tanapat Nunkhong "})});
+app.get('/about', (c) => {return c.json({message: "Mathasit Jaihow"})});
 
 export default app
