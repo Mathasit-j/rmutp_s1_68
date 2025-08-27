b3a9144ec67bedd7774074bb4ffef2c2ab8bcd0f
Mathasit Jaihow <mathasit-j@rmutp.ac.th>
2025-08-27 17:38:09 +0700

    update

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,15 @@
-import { Hono } from "hono";
-const app = new Hono();
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const hono_1 = require("hono");
+const client_1 = require("@prisma/client");
+//import { PrismaClient } from "../generated/prisma/client";
+const prisma = new client_1.PrismaClient();
+const app = new hono_1.Hono();
 app.get('/', (c) => c.text('Hono!'));
 app.get('/about', (c) => { return c.json({ message: "Mathasit Jaihow" }); });
-export default app;
+app.get("/profile", (c) => {
+    //logic
+    const profiles = prisma.profile.findMany();
+    return profiles;
+});
+exports.default = app;
