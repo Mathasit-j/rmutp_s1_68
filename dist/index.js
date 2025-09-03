814eb50d668013be64b51c33d24c895424794e29
Mathasit Jaihow <mathasit-j@rmutp.ac.th>
2025-09-03 16:36:02 +0700

    hash password

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const hono_1 = require("hono");
 const client_1 = require("@prisma/client");
+const bcrypt_1 = require("bcrypt");
 //import { PrismaClient } from "../generated/prisma/client";
 const prisma = new client_1.PrismaClient();
 const app = new hono_1.Hono();
@@ -16,6 +17,11 @@ app.post("/profile", async (c) => {
     //logic to create a new profile
     const body = await c.req.json();
     console.log("input of profile", body);
+    console.log("body.passwaord(original)", body.password);
+    //encode password
+    const passwordHash = await bcrypt_1.default.hash(body.password, 10);
+    console.log("hash.password(after)", passwordHash);
+    //save to db
     //output response
     return c.json({
         message: "create profile completed"
