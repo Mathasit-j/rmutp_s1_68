f49fcd3b16167b78b4a29b34f72b7b85bd29b7b8
Mathasit Jaihow <mathasit-j@rmutp.ac.th>
2025-09-03 16:43:59 +0700

    fix bc

--- app/index.ts
+++ app/index.ts
@@ -1,6 +1,6 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcrypt";
+import * as bcrypt from "bcrypt";
 
 //import { PrismaClient } from "../generated/prisma/client";
 const prisma = new PrismaClient();
